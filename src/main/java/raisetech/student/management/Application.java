188b761b0d7208c4e476e48655ac3bd45fc6b405
gobird <monaka@example.com>
2024-10-06 04:47:00 +0900

    first-commit

---
+++ src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,24 @@
+package raisetech.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+/* localhost:8080/helloを入力すると「Hello World!!!」が表示されます。
+*
+*
+*/
+  @GetMapping("/hello")
+  public String test(){
+    return "Hello World!!!";
+  }
+}
---
+++ src/test/java/raisetech/student/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.student.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
